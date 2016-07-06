e65f99c245f6ae6451e613c630a1a2fd252acd18
Floryan Lollivier <floryan.lollivier@gmail.com>
2016-07-06 18:22:00 +0200

    ajout du dossier js-pangram

---
+++ pangram.js
@@ -0,0 +1,20 @@
+//
+// This is only a SKELETON file for the "Pangram" exercise. It's been provided as a
+// convenience to get you started writing code faster.
+// Make sure to look at pangram.spec.js--that should give you some hints about what is
+// expected here.
+
+'use strict';
+
+function Pangram(textToTest) {
+    this.textToTest = textToTest;
+
+    this.isPangram = function () {
+
+
+        /*
+            Put your code here, you can access the code in variable : this.textToTest
+        */
+
+  }
+}
